591dde841a4ff03f4fac98a61266b41de431ef0c
unknown <alexhoropchanu@gmail.com>
2021-04-08 11:56:06 +0300

    refactor from contextAPI

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,13 +3,20 @@ import Post from "./Post/Post";
 import React from "react";
 
 const MyPosts = (props) => {
-  let PostsElement = props.profilePage.postsData.map((el) => (
+  let PostsElement = props.postsData.map((el) => (
     <Post key={el.id} message={el.message} likeCounts={el.likeCounts} />
   ));
+  let onAddPost = () => {
+    props.addPost();
+  };
+  let onRemovePost = () => {
+    props.removePost();
+  };
   let addPostElem = React.createRef();
-  let onChange = () => {
+  let Change = () => {
+    debugger;
     let text = addPostElem.current.value;
-    props.changePostTextArea(text);
+    props.onChange(text);
   };
   return (
     <div>
@@ -20,14 +27,14 @@ const MyPosts = (props) => {
           rows="10"
           ref={addPostElem}
           className={classes.textarea}
-          onChange={onChange}
+          onChange={Change}
           value={props.profilePage.newPostText}
         />
         <div className={classes.buttons}>
-          <button className={classes.btn} onClick={props.addPost}>
+          <button className={classes.btn} onClick={onAddPost}>
             Add post
           </button>
-          <button className={classes.btn} onClick={props.removePost}>
+          <button className={classes.btn} onClick={onRemovePost}>
             Post remove
           </button>
         </div>
